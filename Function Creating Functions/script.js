8018e65263b89a1b4877a3654d27c3d9e3ead1a9
Carter Lawson <c@rterlawson.com>
2019-05-07 17:08:19 -0400

    clean up formatting

--- Function Creating Functions/script.js
+++ Function Creating Functions/script.js
@@ -1,9 +1,5 @@
-
 function betweenTwoNumbers(n1, n2) {
-
-    //  return m => m > n;
     let returnFunction = (m) => {
-
         if ((m > n1) && (m < n2)) {
             return true;
         } else {
@@ -11,7 +7,7 @@ function betweenTwoNumbers(n1, n2) {
         }
     }
     return returnFunction;
-
 }
+
 let between2And8 = betweenTwoNumbers(2, 8);
 console.log(between2And8(10));
