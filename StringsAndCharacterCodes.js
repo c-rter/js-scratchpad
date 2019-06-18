391cba8e06f7245b56eedf640166050713185b9a
Carter Lawson <c@rterlawson.com>
2019-06-18 14:58:36 -0400

    add character code identifier tests

---
+++ StringsAndCharacterCodes.js
@@ -0,0 +1,7 @@
+let char1 = "Xq";
+console.log(char1.charCodeAt(0));
+console.log(char1.charCodeAt(1));
+console.log(char1.charCodeAt(2));
+console.log(char1.codePointAt(0));
+console.log(char1.codePointAt(1));
+console.log(char1.codePointAt(2));
