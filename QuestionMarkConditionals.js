7f3f8e19d5ca275bb282dd4f5a6b80d02d7cec36
Carter Lawson <c@rterlawson.com>
2019-06-20 10:55:25 -0400

    add '?' conditional testing

---
+++ QuestionMarkConditionals.js
@@ -0,0 +1,20 @@
+let value1 = 4;
+let value2 = 44;
+
+let result1 = value1 > 10 ? "yes" : "no";
+let result2 = value2 > 10 ? "yes" : "no";
+console.log(result1);
+console.log(result2);
+
+console.log(10 > value1 ? "it is": "it is not");
+
+// multi-line '?' conditionals comment out variables for tests
+// let value3 = 25;
+// let value3 = 35;
+ let value3 = 45;
+
+let message = (value3 == 25) ? "25" :
+(value3 == 35) ? "35" :
+"not either 25 or 35";
+
+console.log(message);
