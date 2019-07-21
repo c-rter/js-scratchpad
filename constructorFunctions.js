ec9b5a3bd0ef5d0a6dd7008895db495aeb1b50cd
c-rter <c@rterlawson.com>
2019-07-20 23:00:52 -0400

    add more differentiations:

--- constructorFunctions.js
+++ constructorFunctions.js
@@ -25,5 +25,8 @@ let protoRabbit = {
   let killerRabbit3 = protoRabbit3;
   killerRabbit3.type = "killer3";
   killerRabbit3.speak("SKREEEE!3");
+  console.log(Object.getPrototypeOf(killerRabbit));
   console.log(Object.getPrototypeOf(killerRabbit2));
   console.log(Object.getPrototypeOf(killerRabbit3));
+  console.log(killerRabbit2.speak);
+  console.log(killerRabbit3.speak);
