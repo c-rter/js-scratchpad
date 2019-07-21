14fbe7f2ea58735912b8f9a8ae4e695d25c2a531
c-rter <c@rterlawson.com>
2019-07-20 22:49:05 -0400

    add object construction expiriments

---
+++ constructorFunctions.js
@@ -0,0 +1,27 @@
+let protoRabbit = {
+    speak(line) {
+      console.log(`The ${this.type} rabbit says '${line}'`);
+    }
+  };
+  let killerRabbit = Object.create(protoRabbit);
+  killerRabbit.type = "killer";
+  killerRabbit.speak("SKREEEE!");
+  // → The killer rabbit says 'SKREEEE!'
+
+  let protoRabbit2 = {
+    speak : function(line) {
+      console.log(`The ${this.type} rabbit says '${line}'`);
+    } 
+  };
+  let killerRabbit2 = Object.create(protoRabbit);
+  killerRabbit2.type = "killer2";
+  killerRabbit2.speak("SKREEEE!2");
+
+  let protoRabbit3 = {
+    speak : function(line) {
+      console.log(`The ${this.type} rabbit says '${line}'`);
+    } 
+  };
+  let killerRabbit3 = protoRabbit3;
+  killerRabbit3.type = "killer3";
+  killerRabbit3.speak("SKREEEE!3");
