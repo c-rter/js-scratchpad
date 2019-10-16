21867dcaac0a31c007c0630ab61d541e330b1fbb
Carter Lawson <c@rterlawson.com>
2019-10-16 15:33:39 -0400

    add reduce method

--- map2.js
+++ map2.js
@@ -3,7 +3,14 @@ let newArr = [1, 2, 3, 4, 5, 6, 7];
 let newArr2 = newArr.map(n => n + 2);
 
 console.log(newArr2);
-
+// [ 3, 4, 5, 6, 7, 8, 9 ]
 let newArr3 = newArr2.filter(n => n < 7);
 
 console.log(newArr3);
+// [ 3, 4, 5, 6 ]
+let newArr4 = newArr3.reduce((a, b) => a + " " + b);
+
+console.log(newArr4);
+// 3, 4, 5, 6
+console.log(typeof newArr4);
+// string
