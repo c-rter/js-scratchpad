4150de684dffe0c45c1f75529db0dcf785a95597
Carter Lawson <c@rterlawson.com>
2019-10-21 13:05:04 -0400

    reorient logs

--- roboExample.js
+++ roboExample.js
@@ -1,27 +1,27 @@
 const roads = [
-    "Alice's House-Bob's House", "Alice's House-Cabin",
-    "Alice's House-Post Office", "Bob's House-Town Hall",
-    "Daria's House-Ernie's House", "Daria's House-Town Hall",
-    "Ernie's House-Grete's House", "Grete's House-Farm",
-    "Grete's House-Shop", "Marketplace-Farm",
-    "Marketplace-Post Office", "Marketplace-Shop",
-    "Marketplace-Town Hall", "Shop-Town Hall"
+  "Alice's House-Bob's House", "Alice's House-Cabin",
+  "Alice's House-Post Office", "Bob's House-Town Hall",
+  "Daria's House-Ernie's House", "Daria's House-Town Hall",
+  "Ernie's House-Grete's House", "Grete's House-Farm",
+  "Grete's House-Shop", "Marketplace-Farm",
+  "Marketplace-Post Office", "Marketplace-Shop",
+  "Marketplace-Town Hall", "Shop-Town Hall"
 ];
 
 function buildGraph(edges) {
-    let graph = Object.create(null);
-    function addEdge(from, to) {
-        if (graph[from] == null) {
-            graph[from] = [to];
-        } else {
-            graph[from].push(to);
-        }
+  let graph = Object.create(null);
+  function addEdge(from, to) {
+    if (graph[from] == null) {
+      graph[from] = [to];
+    } else {
+      graph[from].push(to);
     }
-    for (let [from, to] of edges.map(r => r.split("-"))) {
-        addEdge(from, to);
-        addEdge(to, from);
-    }
-    return graph;
+  }
+  for (let [from, to] of edges.map(r => r.split("-"))) {
+    addEdge(from, to);
+    addEdge(to, from);
+  }
+  return graph;
 }
 
 const roadGraph = buildGraph(roads);
@@ -40,65 +40,66 @@ const roadGraph = buildGraph(roads);
   Marketplace: [ 'Farm', 'Post Office', 'Shop', 'Town Hall' ] } */
 
 class VillageState {
-    constructor(place, parcels) {
-        this.place = place;
-        this.parcels = parcels;
-    }
+  constructor(place, parcels) {
+    this.place = place;
+    this.parcels = parcels;
+  }
 
-    move(destination) {
-        if (!roadGraph[this.place].includes(destination)) {
-            return this;
-        } else {
-            let parcels = this.parcels.map(p => {
-                if (p.place != this.place) return p;
-                return { place: destination, address: p.address };
-            });
-        for (let p of parcels) {
-            if (p.place == p.address) {
-                console.log(`Package dropped off: "${JSON.stringify(p)}"`);
-            }
+  move(destination) {
+    if (!roadGraph[this.place].includes(destination)) {
+      return this;
+    } else {
+      let parcels = this.parcels.map(p => {
+        if (p.place != this.place) return p;
+        return { place: destination, address: p.address };
+      });
+      for (let p of parcels) {
+        if (p.place == p.address) {
+          console.log(`Package dropped off: "${JSON.stringify(p)}"`);
         }
-            parcels = parcels.filter(p => p.place != p.address);
+      }
+      parcels = parcels.filter(p => p.place != p.address);
 
-            return new VillageState(destination, parcels);
-        }
+      return new VillageState(destination, parcels);
     }
+  }
 }
 
 function runRobot(state, robot, memory) {
-    for (let turn = 0;; turn++) {
-      if (state.parcels.length == 0) {
-        console.log(`Done in ${turn} turns`);
-        break;
-      }
-      let action = robot(state, memory);
-      state = state.move(action.direction);
-      memory = action.memory;
-      console.log(`Moved to ${action.direction}`);
+  console.log(state);
+  for (let turn = 0; ; turn++) {
+    if (state.parcels.length == 0) {
+      console.log(`Done in ${turn} turns`);
+      break;
     }
+    let action = robot(state, memory);
+    state = state.move(action.direction);
+    memory = action.memory;
+    console.log(`Moved to ${action.direction}`);
   }
+}
 
-  function randomPick(array) {
-    let choice = Math.floor(Math.random() * array.length);
-    return array[choice];
+function randomPick(array) {
+  let choice = Math.floor(Math.random() * array.length);
+  return array[choice];
+}
+
+function randomRobot(state) {
+  return { direction: randomPick(roadGraph[state.place]) };
+}
+
+VillageState.random = function (parcelCount = 5) {
+  let parcels = [];
+  for (let i = 0; i < parcelCount; i++) {
+    let address = randomPick(Object.keys(roadGraph));
+    let place;
+    do {
+      place = randomPick(Object.keys(roadGraph));
+    } while (place == address);
+    parcels.push({ place, address });
   }
-  
-  function randomRobot(state) {
-    return {direction: randomPick(roadGraph[state.place])};
-  }
+  // console.log(parcels);
+  return new VillageState("Post Office", parcels);
+};
 
-  VillageState.random = function(parcelCount = 5) {
-    let parcels = [];
-    for (let i = 0; i < parcelCount; i++) {
-      let address = randomPick(Object.keys(roadGraph));
-      let place;
-      do {
-        place = randomPick(Object.keys(roadGraph));
-      } while (place == address);
-      parcels.push({place, address});
-    }
-    console.log(parcels);
-    return new VillageState("Post Office", parcels);
-  };
-
-  runRobot(VillageState.random(), randomRobot);
+runRobot(VillageState.random(), randomRobot);
