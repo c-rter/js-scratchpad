eaf9bb5d265c7cb72107c2038386b7174e91bd05
Carter Lawson <c@rterlawson.com>
2019-10-23 10:49:06 -0400

    array slicing

---
+++ slice.js
@@ -0,0 +1,5 @@
+var fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+var citrus = fruits.slice(1, 3);
+console.log(`Here are citrus fruits: ${citrus}`);
+var nobanan = fruits.slice(1);
+console.log(`remove the banana and you get:${nobanan}`);
