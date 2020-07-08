e5989826710cee4824890f4e59396e4e82cc002c
c-rter <c@rterlawson.com>
2020-07-08 13:47:08 -0400

    state updater progress

--- robo.js
+++ robo.js
@@ -5,7 +5,7 @@ const roads = [
     "Alice's House-Post Office", "Bob's House-Town Hall",
     "Daria's House-Ernie's House", "Daria's House-Town Hall",
     "Ernie's House-Grete's House", "Grete's House-Farm",
-    "Grete's House-Shop", "Marketplace-Farm",g
+    "Grete's House-Shop", "Marketplace-Farm", 
     "Marketplace-Post Office", "Marketplace-Shop",
     "Marketplace-Town Hall", "Shop-Town Hall"
 ];
@@ -16,23 +16,21 @@ class roadMap {
 
         for (let road of roads) {
 
-            let splitRoad = road.split('-');   
+            let splitRoad = road.split('-');
             let a = splitRoad[0];
             let b = splitRoad[1];
 
             if (this[a] == null) {
                 this[a] = [b];
             }
-            else if (!this[a].includes(b))
-            {
+            else if (!this[a].includes(b)) {
                 this[a].push(b);
             }
 
             if (this[b] == null) {
                 this[b] = [a];
             }
-            else if (!this[b].includes(a))
-            {
+            else if (!this[b].includes(a)) {
                 this[b].push(a);
             }
 
@@ -41,17 +39,42 @@ class roadMap {
 
 }
 
-console.log(new roadMap(roads));
 
-//let first = new VillageState( "Post Office", [{place: "Post Office", address: "Alice's House"}]);
+
+let roadMap1 = new roadMap(roads);
+
+// console.log(new roadMap(roads));
 
 class VillageState {
 
     constructor(place, parcels) {
-
+        this.place = place;
+        this.parcels = parcels;
     }
-    
+
     move(destination) {
+
+        if (!roadMap1[this.place].includes(destination)) {
+            return this;
+        }
+
+        let par = this.parcels.map(n => {
+            console.log(n);
+            if (n.place == this.place) {
+                n.place = destination;
+            }
+
+        }).filter(n => {n.place != n.address});
+
+        return new VillageState(destination, par);
+
 
     }
 }
+
+let first = new VillageState("Post Office", [{ place: "Post Office", address: "Alice's House" }]);
+let next = first.move("Alice's House");
+
+console.log(next.place);      // → Alice's House
+console.log(next.parcels);    // → []
+console.log(first.place);     // → Post Office 
