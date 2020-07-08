c9dd571e016d16f77c3a4f3449772eb516c1ab78
c-rter <c@rterlawson.com>
2020-07-08 11:33:44 -0400

    robo

---
+++ robo.js
@@ -0,0 +1,44 @@
+// convert list of route possibility pairs to destination object
+
+const roads = [
+    "Alice's House-Bob's House", "Alice's House-Cabin",
+    "Alice's House-Post Office", "Bob's House-Town Hall",
+    "Daria's House-Ernie's House", "Daria's House-Town Hall",
+    "Ernie's House-Grete's House", "Grete's House-Farm",
+    "Grete's House-Shop", "Marketplace-Farm",
+    "Marketplace-Post Office", "Marketplace-Shop",
+    "Marketplace-Town Hall", "Shop-Town Hall"
+];
+
+class roadMap {
+
+    constructor(roads) {
+
+        for (let road of roads) {
+
+            let splitRoad = road.split('-');   
+            let a = splitRoad[0];
+            let b = splitRoad[1];
+
+            if (this[a] == null) {
+                this[a] = [b];
+            }
+            else if (!this[a].includes(b))
+            {
+                this[a].push(b);
+            }
+
+            if (this[b] == null) {
+                this[b] = [a];
+            }
+            else if (!this[b].includes(a))
+            {
+                this[b].push(a);
+            }
+
+        }
+    }
+
+}
+
+console.log(new roadMap(roads));
